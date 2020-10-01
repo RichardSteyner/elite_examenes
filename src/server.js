4e217e85411bacb98ea0a37bb7040782a1c7bb36
Steyner <steyner.urupeque.s@gmail.com>
2020-09-30 22:29:05 -0500

    config deploy v2

--- src/server.js
+++ src/server.js
@@ -10,10 +10,10 @@ app.get('/',function(req,res){
 });
 app.listen(process.env.PORT || 8080);*/
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/examenes'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/examenes/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
