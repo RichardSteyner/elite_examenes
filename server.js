d5f2fe40e90fa7d6f8e80633ad800c2a1cc5188e
Steyner <steyner.urupeque.s@gmail.com>
2020-10-01 10:13:43 -0500

    config deploy con fe v9

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+//const express = require('express');
 
 //const path = require('path');
 
-const app = express();
+//const app = express();
 
 /*app.use(express.static(__dirname+'/dist/ng-blog'));
 app.get('/',function(req,res){
@@ -10,7 +10,7 @@ app.get('/',function(req,res){
 });
 app.listen(process.env.PORT || 8080);*/
 // Serve only the static files form the dist directory    
-app.use(express.static('./dist/examenes'));
+/*app.use(express.static('./dist/examenes'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/examenes/'}),
@@ -18,4 +18,20 @@ app.get('/*', (req, res) =>
   
 
 // Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);*/
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/examenes'));
 app.listen(process.env.PORT || 8080);
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/examenes/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
